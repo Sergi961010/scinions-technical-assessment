34476fe7936e070123ac1d9dddbccf8b6cd81e18
sergi-borras <sergi.borras.llaurado@gmail.com>
2024-03-18 20:16:07 +0100

    Minimal requirements for player movement

--- Assets/_Project/Scripts/Player/PlayerMovement.cs
+++ Assets/_Project/Scripts/Player/PlayerMovement.cs
@@ -2,5 +2,29 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float _speed;
+    private float _horizontal;
+    private bool _isFacingRight = true;
+    private SpriteRenderer _spriteRenderer;
 
+    private void Awake()
+    {
+        _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        Flip();
+    }
+
+    private void Update()
+    {
+        _horizontal = Input.GetAxisRaw("Horizontal");
+        transform.Translate(_horizontal * _speed * Time.deltaTime, 0, 0);
+    }
+
+    private void Flip()
+    {
+        if (_isFacingRight && _horizontal < 0 || !_isFacingRight && _horizontal > 0)
+        {
+            _isFacingRight = !_isFacingRight;
+            _spriteRenderer.flipX = !_spriteRenderer.flipX;
+        }
+    }
 }
